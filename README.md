dcb13ef066642742d8a0b353a5cd11150b01a323
gianlucasudano <sudanogianluca@gmail.com>
2021-05-29 11:17:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# react-form-with-useReducer
-Created with CodeSandbox
+# React App template
+
+React App template is a template done starting from codesanbox react template.
+Has additional dependencies like emotion, material-ui, jest, props-type, string-template
+
+## Installation
+
+Download or clone the repo in one folder and install packages
+
+```bash
+      To install dependencies use `npm install`
+```
+
+## Usage
+
+In the folder where the repo is cloned
+
+```bash
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+```
+
+or fork in [Codesanbox](https://codesandbox.io/s/fuer7)
+
+## Goals
+
+The primary goal is to provide a starter template with a basic ecosystem, useful to add styled components using emotion and test code using jest
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import NoSsr from "@material-ui/core/NoSsr";
+import { ThemeProvider } from "@emotion/react";
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider
+} from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+// import isPropValid from "@emotion/is-prop-valid";
+// import styled from "@emotion/styled";
+
+const theme = createMuiTheme();
+
+export default function App() {
+  return (
+    <NoSsr>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Container maxWidth="sm">
+            <Box my={4}>
+              <Typography variant="h4" component="h1" gutterBottom>
+                Simple app template
+              </Typography>
+              <Typography variant="h5" component="h1" gutterBottom>
+                React, emotion, material-ui, jest, props-type, string-template
+              </Typography>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </NoSsr>
+  );
+}
